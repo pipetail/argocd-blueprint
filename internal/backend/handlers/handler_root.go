1c3202bd4613a0e70c01767d47dd81e2a501f374
Stepan Vrany <stepan@vrany.dev>
2020-11-17 23:44:04 +0100

    introduce panic if secrets don't work

--- internal/backend/handlers/handler_root.go
+++ internal/backend/handlers/handler_root.go
@@ -11,7 +11,7 @@ func Root(c *gin.Context) {
 
 	s, err := secret.Get("dev/backend", "AWSCURRENT")
 	if err != nil {
-		fmt.Errorf("could not receive secrets: %s", err)
+		panic(fmt.Sprintf("could not receive secrets: %s", err))
 	}
 
 	c.JSON(200, gin.H{
