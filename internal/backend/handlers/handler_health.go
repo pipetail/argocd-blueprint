fc2101c07fa796b0eb77f0b375c5b057f5accce6
Stepan Vrany <stepan@vrany.dev>
2020-11-18 20:40:09 +0100

    add some dummy checks to readiness probe

--- internal/backend/handlers/handler_health.go
+++ internal/backend/handlers/handler_health.go
@@ -3,17 +3,35 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pipetail/argocd-blueprint/pkg/container"
+	"github.com/pipetail/argocd-blueprint/pkg/server"
 )
 
-func HealthReady(c container.Container) func(c *gin.Context) {
+func HealthReady(container container.Container) func(c *gin.Context) {
 	return func(c *gin.Context) {
+
+		// check if secrets were obtained
+		_, err := container.Secret.GetString("mysqlPassword")
+		if err != nil {
+			server.HandleInternalServerError(c, "could not get mysqlPassword")
+			return
+		}
+
+		_, err = container.Secret.GetString("someToken")
+		if err != nil {
+			server.HandleInternalServerError(c, "could not get someToken")
+			return
+		}
+
+		// todo check connection to DBs etc
+
+		// server is ready
 		c.JSON(200, gin.H{
 			"ready": "ok",
 		})
 	}
 }
 
-func HealthAlive(c container.Container) func(c *gin.Context) {
+func HealthAlive(container container.Container) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"alive": "ok",
