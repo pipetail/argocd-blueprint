808da12a5e91189f2db72410e2c9afe1e94c4672
Stepan Vrany <stepan@vrany.dev>
2020-11-18 12:14:31 +0100

    obtain more secrets

--- internal/backend/handlers/handler_root.go
+++ internal/backend/handlers/handler_root.go
@@ -9,12 +9,19 @@ func Root(secret server.Secret) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		mysqlPassword, err := secret.GetString("mysqlPassword")
 		if err != nil {
-			handleError(c, "could not get configuration")
+			handleError(c, "could not get mysqlPassword")
+			return
+		}
+
+		someToken, err := secret.GetString("someToken")
+		if err != nil {
+			handleError(c, "could not get someToken")
 			return
 		}
 
 		c.JSON(200, gin.H{
 			"password": mysqlPassword,
+			"token":    someToken,
 		})
 	}
 }
